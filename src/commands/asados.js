59c3a4c33f4d7e2b915a1a109db2323154dcb7f7
Jonás Casteglione <jnss.cas@gmail.com>
2021-02-02 14:43:30 -0300

    try/catch added to diego and asados commands

--- src/commands/asados.js
+++ src/commands/asados.js
@@ -3,7 +3,12 @@ const flickrClient = require('../clients/flickr.client');
 module.exports = {
   name: 'asados',
   handle: async (ctx) => {
-    const urlPhoto = await flickrClient.getPhotoRandomByTag('asados');
-    ctx.replyWithPhoto(urlPhoto);
+    try {
+      const urlPhoto = await flickrClient.getPhotoRandomByTag('asados');
+      await ctx.replyWithPhoto(urlPhoto);
+    } catch (e) {
+      console.log(`Error with command ${this.name}: `, e);
+      ctx.reply('No se por qué, pero no puedo mandarte fotos de asados en este momento. Perdoname.');
+    }
   }
 }
--- src/commands/diego.js
+++ src/commands/diego.js
@@ -3,7 +3,12 @@ const flickrClient = require('../clients/flickr.client');
 module.exports = {
   name: 'diego',
   handle: async (ctx) => {
-    const urlPhoto = await flickrClient.getPhotoRandomByTag('diego');
-    ctx.replyWithPhoto(urlPhoto);
+    try {
+      const urlPhoto = await flickrClient.getPhotoRandomByTag('diego');
+      await ctx.replyWithPhoto(urlPhoto);
+    } catch (e) {
+      console.log(`Error with command ${this.name}: `, e);
+      ctx.reply('No se por qué, pero no puedo mandarte fotos del Diego en este momento. Perdoname.');
+    }
   }
 }
