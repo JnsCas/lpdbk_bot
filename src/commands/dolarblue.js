89299f241f9267ccaf6a91385583ab3b1f004321
Jonás Casteglione <jnss.cas@gmail.com>
2023-01-22 13:51:29 -0300

    new dolar blue api

--- src/commands/dolarblue.js
+++ src/commands/dolarblue.js
@@ -3,9 +3,10 @@ const axios = require('axios');
 module.exports = {
   name: 'dolarblue',
   handle: (ctx) => {
-    axios.get('https://api-dolar-argentina.herokuapp.com/api/dolarblue')
+    axios.get('https://www.dolarsi.com/api/api.php?type=valoresprincipales')
       .then((response) => {
-        const result = `💸 Compra: ${response.data.compra} 💸\n💸 Venta: ${response.data.venta} 💸`;
+        const { casa: dolarBlue } = response.data.filter(({ casa }) => casa.nombre.trim().toLowerCase() === "dolar blue")[0]
+        const result = `💸 Compra: ${dolarBlue.compra} 💸\n💸 Venta: ${dolarBlue.venta} 💸`;
         ctx.reply(result, { reply_to_message_id: ctx.message.message_id });
       }).catch((err) => {
         console.error(`Error fetching dollar info: `, err);
